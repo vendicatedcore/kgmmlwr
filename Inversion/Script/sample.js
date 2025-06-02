13e7b8d30a7fa01a5e0d54dc7a0b4615ce3b2620
simmy <143356794+vendicatedcore@users.noreply.github.com>
2025-06-02 23:43:49 +0200

    fix

---
+++ Inversion/Script/sample.js
@@ -0,0 +1,2 @@
+// I have taken down code to avoid people editing variables and re-sharing. The investigation has been closed and every trace of the sample that I was able to track down was reported.
+// Thank you for following through! And thank you to all the people that helped me and provided additional information on the case. I love you, Stay safe! All of you!
